4c816e8b1fe0239dbbdc8635e690da8dc963f8b7
avrland <marcinpopko@Outlook.com>
2018-01-24 10:59:29 +0100

    removed window animations, now toolbar color is on notification bar too

--- app/src/main/java/com/avrland/test5/DrawerActivity.java
+++ app/src/main/java/com/avrland/test5/DrawerActivity.java
@@ -34,7 +34,6 @@ public class DrawerActivity extends AppCompatActivity implements NavigationView.
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
