84a645423358c2f89ac073373794b463b6c6cec3
avrland <marcinpopko@Outlook.com>
2018-01-24 13:27:26 +0100

    KISS stuff

--- app/src/main/java/com/avrland/test5/MainActivity.java
+++ app/src/main/java/com/avrland/test5/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends DrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Activity 1");
     }
 
 
